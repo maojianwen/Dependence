617986e67988b673d3249d5bdb9b002345ce3e88
DESKTOP-IRRGMM3\saic <maojianwen@saicmotor.com>
2018-09-05 10:31:30 +0800

    Initial commit

---
+++ mylibrary/src/main/java/com/dependence/sc/mylibrary/DependenceTest.java
@@ -0,0 +1,13 @@
+package com.dependence.sc.mylibrary;
+
+/**
+ * File description.
+ *
+ * @author saic
+ * @date 2018/9/5
+ */
+public class DependenceTest {
+    public static String getResult(){
+        return "Success!";
+    }
+}
