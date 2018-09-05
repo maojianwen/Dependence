fda39cd9129fe6a79822b144d6f684e599ed0801
DESKTOP-IRRGMM3\saic <maojianwen@saicmotor.com>
2018-09-05 10:47:31 +0800

    commit

---
+++ app/src/main/java/com/dependence/sc/MainCheck.java
@@ -0,0 +1,17 @@
+package com.dependence.sc;
+
+/**
+ * File description.
+ *
+ * @author saic
+ * @date 2018/9/5
+ */
+public class MainCheck {
+
+    /**
+     * @return
+     */
+    public static String getResult() {
+        return "Got";
+    }
+}
